8fefea5c53cc5a0390a468cfc87c072cfe9afc28
guhfelix <gustavoghdp@hotmail.com>
2024-08-27 14:18:50 -0400

    Projeto terminado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo");
-    } 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta!");
+        String numeroConta = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu Nome!");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o Saldo!");
+        double saldo = scanner.nextDouble();
+
+        // Concatenação usando o método concat da classe String
+        String mensagem = "Olá ".concat(nomeCliente)
+                                 .concat(", obrigado por criar uma conta em nosso banco. Sua agência é ")
+                                 .concat(agencia)
+                                 .concat(", conta ")
+                                 .concat(numeroConta)
+                                 .concat(" e seu saldo ")
+                                 .concat(String.valueOf(saldo))
+                                 .concat(" já está disponível para saque.");
+
+        System.out.println(mensagem);
+
+        scanner.close();
+    }
 }
